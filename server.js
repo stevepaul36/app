d2c521daa376a279172e1c653179d2c2fcc39f11
ALLWIN STEVE PAUL <83706309+stevepaul36@users.noreply.github.com>
2024-11-30 13:11:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,43 +10,36 @@ const io = new Server(server);
 // Serve static files from the "public" folder
 app.use(express.static("public"));
 
-// Store connected users
-const users = {};
+// Keep track of connected users and their socket IDs
+let users = {};
 
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
 
-  // Add the connected user to the users object
+  // Add the connected user to the users list
   users[socket.id] = { id: socket.id };
-  console.log("Current users:", users);
 
-  // Send the updated user list to all clients
+  // Send the list of connected users to all clients
   io.emit("update-users", Object.values(users));
 
-  // Handle signaling messages
+  // Handle offer, answer, and ICE candidates (same as before)
   socket.on("offer", (data) => {
-    console.log(`Offer from ${socket.id} to ${data.target}`);
     socket.to(data.target).emit("offer", { sender: socket.id, offer: data.offer });
   });
 
   socket.on("answer", (data) => {
-    console.log(`Answer from ${socket.id} to ${data.target}`);
     socket.to(data.target).emit("answer", { sender: socket.id, answer: data.answer });
   });
 
   socket.on("ice-candidate", (data) => {
-    console.log(`ICE candidate from ${socket.id} to ${data.target}`);
     socket.to(data.target).emit("ice-candidate", { sender: socket.id, candidate: data.candidate });
   });
 
-  // Handle user disconnection
+  // Remove user from the list on disconnect
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
     delete users[socket.id];
-    console.log("Updated users:", users);
-
-    // Notify all clients about the updated user list
-    io.emit("update-users", Object.values(users));
+    io.emit("update-users", Object.values(users)); // Update user list for everyone
   });
 });
 
